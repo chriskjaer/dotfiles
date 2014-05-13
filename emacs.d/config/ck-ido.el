d3c66343f1db3d5e3d53e9e2b05dabba026cc481
chriskjaer <chriskjaer.dk@gmail.com>
2014-05-13 21:56:54 +0200

    removed emacs config

--- emacs.d/config/ck-ido.el
+++
@@ -1,9 +0,0 @@
-(require 'ido)
-(setq ido-enable-flex-matching t) ;; enables fuzzy matching
-(setq ido-everywhere t)
-(setq ido-auto-merge-work-directories-length -1)
-(setq ido-create-new-buffer 'always)
-(setq ido-ignore-extensions t)
-(ido-mode t)
-
-(provide 'ck-ido)
--- emacs.d/config/ck-keybindings.el
+++
@@ -1,7 +0,0 @@
-
-(global-set-key (kbd "M-3") 'split-window-horizontally) ; was digit-argument
-(global-set-key (kbd "M-2") 'split-window-vertically) ; was digit-argument
-(global-set-key (kbd "M-1") 'delete-other-windows) ; was digit-argument
-(global-set-key (kbd "M-0") 'delete-window) ; was digit-argumen
-
-(provide 'ck-keybindings)
