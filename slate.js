a341244aec2a3f097e581f3eccc05c67154fe2b6
chriskjaer <chriskjaer.dk@gmail.com>
2014-09-24 13:25:29 +0200

    updated slate with fullscreen

--- slate.js
+++ slate.js
@@ -1,3 +1,5 @@
+/* global slate */
+
 // Config
 slate.configAll({
   defaultToCurrentScreen: true
@@ -6,13 +8,23 @@ slate.configAll({
 // Operations
 var pushRight = slate.operation('push', {
   direction: 'right',
-  style: 'bar-resize:screenSizeX/2'
+  style:     'bar-resize:screenSizeX/2'
 });
 
 var pushLeft = slate.operation('push', {
   direction: 'left',
-  style: 'bar-resize:screenSizeX/2'
+  style:     'bar-resize:screenSizeX/2'
 });
 
-slate.bind('right:cmd,alt', pushRight);
-slate.bind('left:cmd,alt', pushLeft);
+var fullscreen = slate.operation('move', {
+  x: 'screenOriginX',
+  y: 'screenOriginY',
+  width: 'screenSizeX',
+  height: 'screenSizeY'
+});
+
+slate.bindAll({
+  'right:cmd,alt': pushRight,
+  'left:cmd,alt': pushLeft,
+  'm:cmd,alt': fullscreen
+});
