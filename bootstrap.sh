0c938759b619c9e47c5c3586047bb907f3f37500
chriskjaer <chriskjaer.dk@gmail.com>
2015-10-26 13:53:42 +0100

    Keep sudo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install Hombrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -23,11 +30,11 @@ gem install bundler git-up
 # Install shell themes
 git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 
-# Install rc files - https://github.com/thoughtbot/rcm
-rcup
-
 # Install oh-my-sh
 curl -L http://install.ohmyz.sh | sh
+
+# Install rc files - https://github.com/thoughtbot/rcm
+rcup
 
 # Install osx settings
 source './osx'
