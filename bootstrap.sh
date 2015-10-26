83fb48926a9606c5c599e284d4ab888a18416ad1
chriskjaer <chriskjaer.dk@gmail.com>
2015-10-26 16:19:42 +0100

    Added ruby install step

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,23 +9,10 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Install Hombrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
 brew tap Homebrew/bundle
 brew bundle
-
-# Install latest npm
-npm install -g n
-
-# Install lastest node
-n latest
-
-# Install latest npm
-npm install -g npm
-
-# Global npm modules
-npm install -g standard eslint-config-standard eslint-plugin-standard
-
-# Global Gems
-gem install bundler git-up
+brew cleanup
 
 # Install shell themes
 git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
@@ -35,6 +22,20 @@ curl -L http://install.ohmyz.sh | sh
 
 # Install rc files - https://github.com/thoughtbot/rcm
 rcup
+
+# Node
+npm install -g n
+n latest # latest node
+npm install -g npm # make sure we have lastest npm before we do anything
+npm install -g standard eslint-config-standard eslint-plugin-standard
+
+# Ruby
+LATEST_RUBY=$(rbenv install -l | grep -v - | tail -1)
+rbenv install $LATEST_RUBY
+rbenv global $LATEST_RUBY
+rbenv rehash
+gem install bundler git-up
+
 
 # Install osx settings
 source './osx'
