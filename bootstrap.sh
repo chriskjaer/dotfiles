6321ae1ec9644c9f060295003ef0644b89b9d648
chriskjaer <chriskjaer.dk@gmail.com>
2015-10-26 12:41:35 +0100

    Add bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+# Install Hombrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brews
+brew brew tap Homebrew/bundle
+brew bundle Brewfile
+brew bundle Caskfile
+
+# Install latest npm
+npm install -g n
+
+# Install lastest node
+n latest
+
+# Install latest npm
+npm install -g npm
+
+# Global npm modules
+npm install -g standard eslint-config-standard eslint-plugin-standard
+
+# Global Gems
+gem install bundler git-up
+
+# Install shell themes
+git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+
+# Install rc files - https://github.com/thoughtbot/rcm
+rcup
+
+# Install oh-my-sh
+curl -L http://install.ohmyz.sh | sh
+
+# Install osx settings
+source './osx'
